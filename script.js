f982a4b6f625b5868caa20ca384fb118e3a02de3
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-02 21:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
-	const body=document.body);
+	const body=document.body;
 	const para=document.createElement("p");
 	para.innerHTML="DOM load success";
 	body.appendChild(para);
